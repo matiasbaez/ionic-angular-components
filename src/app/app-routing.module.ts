9e0042dc1377c18bc7fcf01aa0f885b07e52e0a9
Matias Báez <matiasbaez@users.noreply.github.com>
2021-04-05 19:07:17 -0400

    Added, fab page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,6 +11,7 @@ const routes: Routes = [
   { path: 'card', loadChildren: () => import('./pages/card/card.module').then( m => m.CardPageModule) },
   { path: 'check', loadChildren: () => import('./pages/check/check.module').then( m => m.CheckPageModule) },
   { path: 'date-time', loadChildren: () => import('./pages/date-time/date-time.module').then( m => m.DateTimePageModule) },
+  { path: 'fab', loadChildren: () => import('./pages/fab/fab.module').then( m => m.FabPageModule) },
   { path: 'tabs', loadChildren: () => import('./pages/tabs/tabs.module').then( m => m.TabsPageModule) },
 ];
 
