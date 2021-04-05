33fea36675be1cef16e94a71261334cb9b5ca581
Matias Báez <matiasbaez@users.noreply.github.com>
2021-04-05 19:11:13 -0400

    Added, popover page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -19,6 +19,7 @@ const routes: Routes = [
   { path: 'list-reorder', loadChildren: () => import('./pages/list-reorder/list-reorder.module').then( m => m.ListReorderPageModule) },
   { path: 'loading', loadChildren: () => import('./pages/loading/loading.module').then( m => m.LoadingPageModule) },
   { path: 'modal', loadChildren: () => import('./pages/modal/modal.module').then( m => m.ModalPageModule) },
+  { path: 'popover', loadChildren: () => import('./pages/popover/popover.module').then( m => m.PopoverPageModule) },
   { path: 'tabs', loadChildren: () => import('./pages/tabs/tabs.module').then( m => m.TabsPageModule) },
 ];
 
