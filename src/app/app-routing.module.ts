955e667f1a4c3b23e33cee0a6ae9335f82c85a76
Matias Báez <matiasbaez@users.noreply.github.com>
2021-04-05 19:05:36 -0400

    Added, avatar page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ const routes: Routes = [
   { path: 'inicio', loadChildren: () => import('./pages/inicio/inicio.module').then( m => m.InicioPageModule) },
   { path: 'alert', loadChildren: () => import('./pages/alert/alert.module').then( m => m.AlertPageModule) },
   { path: 'action-sheet', loadChildren: () => import('./pages/action-sheet/action-sheet.module').then( m => m.ActionSheetPageModule) },
+  { path: 'avatar', loadChildren: () => import('./pages/avatar/avatar.module').then( m => m.AvatarPageModule) },
   { path: 'tabs', loadChildren: () => import('./pages/tabs/tabs.module').then( m => m.TabsPageModule) },
 ];
 
