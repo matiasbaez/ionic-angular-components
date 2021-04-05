e119fdccc4b6ca5cbc68b3890849001fe7a01736
Matias Báez <matiasbaez@users.noreply.github.com>
2021-04-05 19:12:49 -0400

    Added, slides page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -24,6 +24,7 @@ const routes: Routes = [
   { path: 'refresher', loadChildren: () => import('./pages/refresher/refresher.module').then( m => m.RefresherPageModule) },
   { path: 'search-bar', loadChildren: () => import('./pages/search-bar/search-bar.module').then( m => m.SearchBarPageModule) },
   { path: 'segment', loadChildren: () => import('./pages/segment/segment.module').then( m => m.SegmentPageModule) },
+  { path: 'slides', loadChildren: () => import('./pages/slides/slides.module').then( m => m.SlidesPageModule) },
   { path: 'tabs', loadChildren: () => import('./pages/tabs/tabs.module').then( m => m.TabsPageModule) },
 ];
 
