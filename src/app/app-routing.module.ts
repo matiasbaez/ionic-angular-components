f1bb3d89ff3a0d5094087377f98aee4cc744c61c
Matias Báez <matiasbaez@users.noreply.github.com>
2021-04-05 19:04:41 -0400

    Added, alert page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'inicio', pathMatch: 'full' },
   { path: 'inicio', loadChildren: () => import('./pages/inicio/inicio.module').then( m => m.InicioPageModule) },
+  { path: 'alert', loadChildren: () => import('./pages/alert/alert.module').then( m => m.AlertPageModule) },
   { path: 'tabs', loadChildren: () => import('./pages/tabs/tabs.module').then( m => m.TabsPageModule) },
 ];
 
